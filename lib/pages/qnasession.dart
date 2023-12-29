8682dba5a6eafe58e75c121c15233d25877bb25a
Regal Adhikari <65963700+RegalAdhikari@users.noreply.github.com>
2023-12-29 17:39:52 +0545

    quick fix

--- lib/pages/qnasession.dart
+++ lib/pages/qnasession.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+//This is depreciated use chatapp.art instead
 class qnasession extends StatefulWidget {
   const qnasession({super.key});
 
@@ -18,7 +19,7 @@ class _qnasessionState extends State<qnasession> {
 
     final response = await http.post(
       Uri.parse(
-          'http://192.168.1.78:9000/ask_question'), // Gotta replace this when I deploy this to heroki
+          'http://192.168.1.75:9000/ask_question'), // Gotta replace this when I deploy this to heroki
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
